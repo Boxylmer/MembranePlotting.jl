9743d835f68e663e6b6a27aa741fd6cae98d23b3
Documenter.jl <documenter@juliadocs.github.io>
2023-07-07 04:34:54 +0000

    build based on 9fe6662

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MembranePlotting","category":"page"},{"location":"#MembranePlotting","page":"Home","title":"MembranePlotting","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MembranePlotting.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MembranePlotting]","category":"page"}]
+}
